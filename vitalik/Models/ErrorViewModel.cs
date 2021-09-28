ebb6d7657da9e8aab097ce0ae6a3fb013483e5d4
RomanLypenko <87646932+RomanLypenko@users.noreply.github.com>
2021-09-28 20:18:17 +0300

    1

---
+++ vitalik/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vitalik.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
